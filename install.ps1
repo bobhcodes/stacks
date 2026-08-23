f6cd81c4fc1e82e00841a5d1b23af8e78552ae15
bob h <mail@bob.codes>
2026-08-23 22:49:59 +0100

    tidying install.ps1 script
    alphabetising, and adding LEGOEducationSPIKE, Logitech.CameraSettings, Ollama, and RetroArch

--- install.ps1
+++ install.ps1
@@ -1,46 +1,48 @@
 function Add-Program([string] $Id) {
-	winget install --accept-package-agreements --accept-source-agreements --exact --id "$Id"
+	winget install --accept-package-agreements --accept-source-agreements --exact --id "$Id" --verbose
 }
 
-#Add-Program '3Dconnexion.3DxWare.10'
-Add-Program '7zip.7zip'
-Add-Program 'Brother.iPrintScan'
-Add-Program 'CodecGuide.K-LiteCodecPack.Standard'
-Add-Program 'Discord.Discord'
-Add-Program 'Docker.DockerDesktop'
-Add-Program 'DominikReichl.KeePass'
-#Add-Program 'ElectronicArts.EADesktop'
-Add-Program 'Exercism.CLI'
-Add-Program 'Fork.Fork'
-Add-Program 'Gyan.FFmpeg'
-Add-Program 'IDRIX.VeraCrypt'
-Add-Program 'jqlang.jq'
-Add-Program 'LibreWolf.LibreWolf'
-#Add-Program 'MusicBrainz.Picard'
-Add-Program 'Mythicsoft.AgentRansack'
-Add-Program 'Notepad++.Notepad++'
-Add-Program 'OBSProject.OBSStudio'
-#Add-Program 'OpenRGB.OpenRGB'
-#Add-Program 'Oracle.MySQLWorkbench'
-#Add-Program 'Piriform.CCleaner'
-Add-Program 'Piriform.Defraggler'
-Add-Program 'Proton.ProtonVPN'
-Add-Program 'RaspberryPiFoundation.RaspberryPiImager'
-Add-Program 'rocksdanister.LivelyWallpaper'
-Add-Program 'Rufus.Rufus'
-Add-Program 'ShareX.ShareX'
-Add-Program 'Valve.Steam'
-Add-Program 'WinDirStat.WinDirStat'
-#Add-Program 'Xencelabs.Driver'
+Add-Program '7zip.7zip' # 7-Zip
+Add-Program 'Mythicsoft.AgentRansack' # Agent Ransack
+Add-Program 'Brother.iPrintScan' # Brother iPrint&Scan
+#Add-Program 'Piriform.CCleaner' # CCleaner
+Add-Program 'Piriform.Defraggler' # Defraggler
+Add-Program 'Discord.Discord' # Discord
+Add-Program 'Docker.DockerDesktop' # Docker Desktop
+Add-Program 'Exercism.CLI' # Exercism CLI
+Add-Program 'Gyan.FFmpeg' # FFmpeg
+Add-Program 'Fork.Fork' # Fork
+Add-Program 'jqlang.jq' # jq
+Add-Program 'DominikReichl.KeePass' # KeePass Password Safe
+Add-Program 'CodecGuide.K-LiteCodecPack.Standard' # K-Lite Codec Pack tandard
+Add-Program 'LEGO.LEGOEducationSPIKE' # LEGO Education SPIKE
+Add-Program 'LibreWolf.LibreWolf' # LibreWolf
+Add-Program 'rocksdanister.LivelyWallpaper' # Lively Wallpaper
+Add-Program 'Logitech.CameraSettings' # Logitech Camera Settings
+Add-Program 'MusicBrainz.Picard' # MusicBrainz Picard
+Add-Program 'Oracle.MySQLWorkbench' # MySQL Workbench
+Add-Program 'Notepad++.Notepad++' # Notepad++
+Add-Program 'OBSProject.OBSStudio' # OBS Studio
+Add-Program 'Ollama.Ollama' # Ollama
+Add-Program 'OpenRGB.OpenRGB' # OpenRGB
+Add-Program 'Proton.ProtonVPN' # Proton VPN
+Add-Program 'RaspberryPiFoundation.RaspberryPiImager' # Raspberry Pi Imager
+Add-Program 'Libretro.RetroArch' # RetroArch
+Add-Program 'Rufus.Rufus' # Rufus
+Add-Program 'ShareX.ShareX' # ShareX
+Add-Program 'Valve.Steam' # Steam
+Add-Program 'IDRIX.VeraCrypt' # VeraCrypt
+Add-Program 'WinDirStat.WinDirStat' # WinDirStat
 
-#Add-Program 'Elgato.CameraHub'
-Add-Program 'Elgato.StreamDeck'
-#Add-Program 'Elgato.WaveLink'
+#Add-Program 'Elgato.CameraHub' # Elgato Camera Hub
+Add-Program 'Elgato.StreamDeck' # Elgato Stream Deck
+#Add-Program 'Elgato.WaveLink' # Elgato Wave Link
 
-Add-Program 'Microsoft.Git'
-Add-Program 'Microsoft.PowerShell'
-Add-Program 'Microsoft.Sysinternals.RDCMan'
-Add-Program 'Microsoft.Teams'
-Add-Program 'Microsoft.VisualStudio.Community'
-Add-Program 'Microsoft.VisualStudioCode'
-Add-Program 'Microsoft.WindowsTerminal'
+Add-Program 'Microsoft.Git' # Git
+Add-Program 'Microsoft.PowerShell' # PowerShell 7-x64
+Add-Program 'Microsoft.Sysinternals.RDCMan' # Remote Desktop Connection Manager
+Add-Program 'Microsoft.Teams' # Microsoft Teams
+Add-Program 'Microsoft.VisualStudio.Community' # Visual Studio Community
+Add-Program 'Microsoft.VisualStudioCode' # Microsoft Visual Studio Code
+Add-Program 'Microsoft.WSL' # Windows Subsystem for Linux
+Add-Program 'Microsoft.WindowsTerminal' # Windows Terminal
